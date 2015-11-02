8239da127794cfbda8ba5fa953fd1474f562dd1f
Florian Purchess <florian@purchess.de>
2015-11-02 23:08:22 +0100

    testing editor command handlers

--- src/blank/plugins/editor/editor_test.go
+++ src/blank/plugins/editor/editor_test.go
@@ -3,14 +3,27 @@ package editor
 import (
 	"blank"
 	"testing"
+
+	"github.com/facebookgo/ensure"
 )
 
 func TestInitRegistersHandler(t *testing.T) {
 	e := NewEditor()
 
+	// TODO mock blank
 	b := blank.NewBlank("", false, "", []blank.Plugin{})
 	b.Tunnel = blank.NewTunnel(nil)
 	e.Init(b)
 
 	// TODO assertion
+}
+
+func TestOnSave(t *testing.T) {
+	e := NewEditor()
+	ensure.Nil(t, e.onSave(&blank.Command{}))
+}
+
+func TestOnOpen(t *testing.T) {
+	e := NewEditor()
+	ensure.Nil(t, e.onOpen(&blank.Command{}))
 }
