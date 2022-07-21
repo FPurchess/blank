ddbb4fe8cb44b372cc19021d7eb1259c9d97db6c
Florian Purchess <florian@purchess.de>
2022-07-21 19:44:53 +0200

    refactor(tauri): drop tauri command `export_as_pdf`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,6 @@
 )]
 
 use std::fs;
-use std::sync::mpsc::{sync_channel, SyncSender};
-use std::thread;
-use wkhtmltopdf::*;
-
-struct BlankState(SyncSender<(String, String)>);
 
 #[tauri::command]
 fn open_file(path: String) -> String {
@@ -20,54 +15,10 @@ fn save_file(path: String, content: String) {
     fs::write(path, content).expect("failed to write file");
 }
 
-#[tauri::command]
-fn export_as_pdf(path: String, content: String, state: tauri::State<BlankState>) {
-    state
-        .0
-        .send((path, content))
-        .expect("failed to send PDF export")
-}
-
-fn init_pdf_application() -> SyncSender<(String, String)> {
-    let (sender, receiver) = sync_channel(0);
-
-    thread::spawn(move || {
-        let pdf_app = PdfApplication::new().expect("Failed to initialize PDF Application");
-
-        loop {
-            let (path, content) = receiver
-                .recv()
-                .expect("[pdf_thread]: pdf thread could not receive data");
-
-            let path: String = path;
-            let content: String = content;
-
-            let mut pdfout = pdf_app
-                .builder()
-                .page_size(PageSize::A4)
-                .orientation(Orientation::Portrait)
-                .margin(Size::Millimeters(15))
-                .build_from_html(&content)
-                .expect("failed to build pdf");
-
-            pdfout.save(path).expect("failed to save PDF");
-        }
-    });
-
-    sender
-}
-
 fn main() {
-    let sender = init_pdf_application();
-
     let context = tauri::generate_context!();
     tauri::Builder::default()
-        .manage(BlankState(sender))
-        .invoke_handler(tauri::generate_handler![
-            open_file,
-            save_file,
-            export_as_pdf
-        ])
+        .invoke_handler(tauri::generate_handler![open_file, save_file])
         .menu(if cfg!(target_os = "macos") {
             tauri::Menu::os_default(&context.package_info().name)
         } else {
