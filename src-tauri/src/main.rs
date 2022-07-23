e155d088e968ddba9df90633dafa08856649fe98
Florian Purchess <florian@purchess.de>
2022-07-23 08:15:59 +0200

    refactor(tauri): drop deprecated tauri commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,22 +3,9 @@
     windows_subsystem = "windows"
 )]
 
-use std::fs;
-
-#[tauri::command]
-fn open_file(path: String) -> String {
-    return fs::read_to_string(path).expect("failed to read file");
-}
-
-#[tauri::command]
-fn save_file(path: String, content: String) {
-    fs::write(path, content).expect("failed to write file");
-}
-
 fn main() {
     let context = tauri::generate_context!();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![open_file, save_file])
         .menu(if cfg!(target_os = "macos") {
             tauri::Menu::os_default(&context.package_info().name)
         } else {
