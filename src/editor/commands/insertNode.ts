789e3ea0c94bf6544ac29dab0f67672cc9735d89
Florian Purchess <florian@purchess.de>
2022-07-25 22:28:41 +0200

    chore(commands): better typing

--- src/editor/commands/insertNode.ts
+++ src/editor/commands/insertNode.ts
@@ -1,11 +1,10 @@
 import { NodeSpec } from 'prosemirror-model';
-import { Command } from 'prosemirror-state';
+import { Command, Transaction } from 'prosemirror-state';
 import { chainCommands, exitCode } from 'prosemirror-commands';
 
 export default (node: NodeSpec): Command =>
-  chainCommands(exitCode, (state, dispatch) => {
+  chainCommands(exitCode, (state, dispatch: (tr: Transaction) => void) => {
     if (dispatch) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       dispatch(state.tr.replaceSelectionWith(node.create()).scrollIntoView());
     }
     return true;
