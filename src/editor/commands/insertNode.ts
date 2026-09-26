c65838acb24e18c5c4564f7b2a70d53451de0c8e
Florian Purchess <FPurchess@users.noreply.github.com>
2026-09-26 11:53:30 +0200

    chore: remove dead code, share error formatting and tidy CI (#39)
    * chore: remove dead code, share error formatting and tidy CI
    
    * chore: point the release notes at the install guide

--- src/editor/commands/insertNode.ts
+++ src/editor/commands/insertNode.ts
@@ -1,8 +1,8 @@
-import { type NodeSpec } from "prosemirror-model";
+import type { NodeType } from "prosemirror-model";
 import type { Command } from "prosemirror-state";
 import { chainCommands, exitCode } from "prosemirror-commands";
 
-export default (node: NodeSpec): Command =>
+export default (node: NodeType): Command =>
   chainCommands(exitCode, (state, dispatch) => {
     if (dispatch) {
       dispatch(state.tr.replaceSelectionWith(node.create()).scrollIntoView());
